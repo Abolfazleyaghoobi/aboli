a27ec86efc5253be12e1943e2275b7769c7dc5f8
Abolfazle <80malard57@gmail.com>
2026-05-23 16:49:03 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    // port: 5057,
-    open: true,
-    host:true
-  },
-  base:'/Portfolio/'
+
+  base:'/aboli/'
 
 })
